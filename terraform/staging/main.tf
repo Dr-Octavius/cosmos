3f78972cf445bbd21531a3890ff74c53293fcb43
CaspianMike-L <looweesing@gmail.com>
2025-04-25 20:59:19 +0800

    nil: cosmetic changes

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -19,8 +19,8 @@ locals {
 # - resource blocks only
 #------------------------
 # Sefire Staging Environment using DOKS
-resource "digitalocean_kubernetes_cluster" "sefire_sgp1_dev" {
-  name    = "sefire-sgp1-dev"
+resource "digitalocean_kubernetes_cluster" "cluster" {
+  name    = "sefire-sgp1-staging"
   region  = "sgp1"
   version = "1.32.2-do.0"
 
@@ -44,43 +44,43 @@ resource "digitalocean_kubernetes_cluster" "sefire_sgp1_dev" {
 # Module Config for dedicated Prometheus nodepool
 module "prometheus_np" {
   source     = "./modules/nodepool"
-  cluster_id = digitalocean_kubernetes_cluster.sefire_sgp1_dev.id
+  cluster_id = digitalocean_kubernetes_cluster.cluster.id
   name       = "prometheus-np"
   size       = local.medium_size
   auto_scale = true
   min_nodes  = 1
   max_nodes  = 3
-  depends_on = [digitalocean_kubernetes_cluster.sefire_sgp1_dev]
+  depends_on = [digitalocean_kubernetes_cluster.cluster]
 }
 
 # Module Config for dedicated Jaeger nodepool
 module "jaeger_np" {
   source     = "./modules/nodepool"
-  cluster_id = digitalocean_kubernetes_cluster.sefire_sgp1_dev.id
+  cluster_id = digitalocean_kubernetes_cluster.cluster.id
   name       = "jaeger-np"
   size       = local.medium_size
   auto_scale = true
   min_nodes  = 1
   max_nodes  = 3
-  depends_on = [digitalocean_kubernetes_cluster.sefire_sgp1_dev]
+  depends_on = [digitalocean_kubernetes_cluster.cluster]
 }
 
 # Module Config for dedicated Elasticsearch nodepool
 module "elasticsearch_np" {
   source     = "./modules/nodepool"
-  cluster_id = digitalocean_kubernetes_cluster.sefire_sgp1_dev.id
+  cluster_id = digitalocean_kubernetes_cluster.cluster.id
   name       = "elasticsearch-np"
   size       = local.general_size
   auto_scale = true
   min_nodes  = 1
   max_nodes  = 3
-  depends_on = [digitalocean_kubernetes_cluster.sefire_sgp1_dev]
+  depends_on = [digitalocean_kubernetes_cluster.cluster]
 }
 
 # Module Config for Sefire Firewall
 module "sefire_sgp1_dev_firewall" {
-  source     = "./modules/firewall"
-  name       = "sefire-sgp1-dev-firewall"
+  source = "./modules/firewall"
+  name   = "sefire-sgp1-dev-firewall"
   inbound_rules = [
     {
       protocol         = "tcp"
@@ -120,5 +120,5 @@ module "sefire_sgp1_dev_firewall" {
       destination_addresses = ["0.0.0.0/0"]
     }
   ]
-  depends_on = [digitalocean_kubernetes_cluster.sefire_sgp1_dev]
+  depends_on = [digitalocean_kubernetes_cluster.cluster]
 }
--- terraform/staging/outputs.tf
+++ terraform/staging/outputs.tf
@@ -4,6 +4,6 @@
 
 output "kubeconfig" {
   description = "Kubernetes Credentials Output"
-  value     = digitalocean_kubernetes_cluster.sefire_sgp1_dev.kube_config.0.raw_config
-  sensitive = true
+  value       = digitalocean_kubernetes_cluster.sefire_sgp1_dev.kube_config.0.raw_config
+  sensitive   = true
 }
